ef1abb91e5ddcd518f40795eace8d51a0f365aa9
Mateus Zanetti Camargo Penteado <63674601+Matezcp@users.noreply.github.com>
2020-07-04 23:58:01 -0300

    New Urls added

--- src/inglesfacil/SetupSubject.java
+++ src/inglesfacil/SetupSubject.java
@@ -32,29 +32,29 @@ public class SetupSubject {
         if (category.equals("Animais")) {
             if (sub.equals("Mamíferos")) {
                 subject.setName("Dog", "Cachorro");
-                subject.setImage("Dog", new Image("/resources/animals/mamiferos/dog.jpg"));
+                subject.setImage("Dog", new Image("/resources/animals/mamiferos/dog.png"));
                 subject.setName("Cat", "Gato");
                 subject.setImage("Cat", new Image("/resources/animals/mamiferos/cat.png"));
                 subject.setName("Horse", "Cavalo");
-                subject.setImage("Horse", new Image("/resources/animals/mamiferos/horse.jpg"));
+                subject.setImage("Horse", new Image("/resources/animals/mamiferos/horse.png"));
                 subject.setName("Monkey", "Macaco");
-                subject.setImage("Monkey", new Image("/resources/animals/mamiferos/monkey.jpg"));
+                subject.setImage("Monkey", new Image("/resources/animals/mamiferos/monkey.png"));
             } else if (sub.equals("Aquáticos")) {
                 subject.setName("Octopus", "Polvo");
-                subject.setImage("Octopus", new Image("/resources/animals/aquaticos/octopus.jpg"));
+                subject.setImage("Octopus", new Image("/resources/animals/aquaticos/octopus.png"));
                 subject.setName("Whale", "Baleia");
                 subject.setImage("Whale", new Image("/resources/animals/aquaticos/whale.png"));
                 subject.setName("Shark", "Tubarão");
                 subject.setImage("Shark", new Image("/resources/animals/aquaticos/shark.png"));
                 subject.setName("Fish", "Peixe");
-                subject.setImage("Fish", new Image("/resources/animals/aquaticos/fish.jpg"));
+                subject.setImage("Fish", new Image("/resources/animals/aquaticos/fish.png"));
             } else if (sub.equals("Selvagens")) {
                 subject.setName("Lion", "Leão");
-                subject.setImage("Lion", new Image("/resources/animals/selvagens/lion.jpg"));
+                subject.setImage("Lion", new Image("/resources/animals/selvagens/lion.png"));
                 subject.setName("Tiger", "Tigre");
-                subject.setImage("Tiger", new Image("/resources/animals/selvagens/tiger.jpg"));
+                subject.setImage("Tiger", new Image("/resources/animals/selvagens/tiger.png"));
                 subject.setName("Elephant", "Elefante");
-                subject.setImage("Elephant", new Image("/resources/animals/selvagens/elephant.jpg"));
+                subject.setImage("Elephant", new Image("/resources/animals/selvagens/elephant.png"));
                 subject.setName("Bear", "Urso");
                 subject.setImage("Bear", new Image("/resources/animals/selvagens/bear.png"));
             }
@@ -90,40 +90,40 @@ public class SetupSubject {
                 subject.setName("Pen", "Caneta");
                 subject.setImage("Pen", new Image("/resources/object/escola/pen.png"));
                 subject.setName("Pencil", "Lápis");
-                subject.setImage("Pencil", new Image("/resources/object/escola/pencil.jpg"));
+                subject.setImage("Pencil", new Image("/resources/object/escola/pencil.png"));
                 subject.setName("Book", "Livro");
-                subject.setImage("Book", new Image("/resources/object/escola/book.jpg"));
+                subject.setImage("Book", new Image("/resources/object/escola/book.gif"));
                 subject.setName("Notebook", "Caderno");
                 subject.setImage("Notebook", new Image("/resources/object/escola/notebook.png"));
             } else if (sub.equals("Tecnologia")) {
                 subject.setName("Mouse", "Mouse");
                 subject.setImage("Mouse", new Image("/resources/object/tecnologia/mouse.jpg"));
                 subject.setName("Keyboard", "Teclado");
-                subject.setImage("Keyboard", new Image("/resources/object/tecnologia/keyboard.jpg"));
+                subject.setImage("Keyboard", new Image("/resources/object/tecnologia/keyboard.png"));
                 subject.setName("Cellphone", "Celular");
-                subject.setImage("Cellphone", new Image("/resources/object/tecnologia/cellphone.jpeg"));
+                subject.setImage("Cellphone", new Image("/resources/object/tecnologia/cellphone.png"));
                 subject.setName("Computer", "Computador");
-                subject.setImage("Computer", new Image("/resources/object/tecnologia/computer.jpg"));
+                subject.setImage("Computer", new Image("/resources/object/tecnologia/computer.gif"));
             } else if (sub.equals("Móveis")) {
                 subject.setName("Chair", "Cadeira");
-                subject.setImage("Chair", new Image("/resources/object/moveis/chair.jpg"));
+                subject.setImage("Chair", new Image("/resources/object/moveis/chair.png"));
                 subject.setName("Desk", "Mesa");
                 subject.setImage("Desk", new Image("/resources/object/moveis/desk.png"));
                 subject.setName("Bed", "Cama");
-                subject.setImage("Bed", new Image("/resources/object/moveis/bed.jpg"));
+                subject.setImage("Bed", new Image("/resources/object/moveis/bed.png"));
                 subject.setName("Couch", "Sofá");
-                subject.setImage("Couch", new Image("/resources/object/moveis/couch.jpg"));
+                subject.setImage("Couch", new Image("/resources/object/moveis/couch.png"));
             }else if (sub.equals("Cozinha")) {
                 subject.setName("Knife", "faca");
-                subject.setImage("Knife", new Image("/resources/object/cozinha/knife.jpg"));
+                subject.setImage("Knife", new Image("/resources/object/cozinha/knife.png"));
                 subject.setName("Fork", "Garfo");
-                subject.setImage("Fork", new Image("/resources/object/cozinha/fork.jpg"));
+                subject.setImage("Fork", new Image("/resources/object/cozinha/fork.png"));
                 subject.setName("Spoon", "Colher");
-                subject.setImage("Spoon", new Image("/resources/object/cozinha/spoon.jpg"));
+                subject.setImage("Spoon", new Image("/resources/object/cozinha/spoon.png"));
                 subject.setName("Plate", "Prato");
-                subject.setImage("Plate", new Image("/resources/object/cozinha/plate.jpg"));
+                subject.setImage("Plate", new Image("/resources/object/cozinha/plate.png"));
                 subject.setName("Kitchen", "Cozinha");
-                subject.setImage("Kitchen", new Image("/resources/object/cozinha/kitchen.jpg"));
+                subject.setImage("Kitchen", new Image("/resources/object/cozinha/kitchen.png"));
             }
         }
         return subject;
